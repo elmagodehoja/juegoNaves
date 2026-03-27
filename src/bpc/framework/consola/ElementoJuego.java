edf217103f248a76e316cf0f991c0c92cc2f451f
coronel845sagaz <angelpuert500@gmail.com>
2026-03-27 22:08:03 +0100

    interfaz ElementoJuego y clase GameObject (faltan los imports de las otras clases)

---
+++ src/bpc/framework/consola/ElementoJuego.java
@@ -0,0 +1,7 @@
+package bpc.framework.consola;
+
+public interface ElementoJuego {
+    public abstract void inicializar();
+    public abstract void ejecutarFrame();
+    public abstract void finalizar();
+}
