189e38f2b968a57b184b4f21f856a7788b123887
sarajimenezlara <sarajimlar207@gmail.com>
2026-03-28 17:18:24 +0100

    añadir clase Escena (faltan cosas requeridas de otras clases)

--- src/bpc/framework/consola/ElementoJuego.java
+++ src/bpc/framework/consola/ElementoJuego.java
@@ -2,6 +2,8 @@ package bpc.framework.consola;
 
 public interface ElementoJuego {
     public abstract void inicializar();
+    void añadir(GameObject obj);
+    void retirar(GameObject obj);
     public abstract void ejecutarFrame();
     public abstract void finalizar();
 }
