cf95a3a31ccb8524a448cf2e19adddcd358302c1
Luis <luiscampillo07.com>
2026-03-29 11:17:55 +0200

    Clase Juego aun incompleta, me falta la clase Resolucion.

---
+++ src/bpc/framework/consola/Juego.java
@@ -0,0 +1,52 @@
+package bpc.framework.consola;
+
+import bpc.daw.consola.Consola;
+import bpc.daw.consola.Teclado;
+
+import java.awt.event.KeyEvent;
+
+public class Juego {
+    private Consola consola;
+    private Escena escena;
+    private Resolucion resolucion;
+    private  boolean detener;
+
+    public Juego(){
+    }
+
+    public void iniciar(Escena e, Resolucion r){
+        this.consola = new Consola(r.getResolucion());
+        this.resolucion = r;
+        this.setEscena(e);
+        this.detener = false;
+
+        Teclado teclado = this.consola.getTeclado();
+        while(detener == false && teclado.teclaPulsada(KeyEvent.VK_ESCAPE) == false){
+            this.escena.ejecutarFrame();
+            this.consola.esperarSiguienteFrame();
+        }
+        this.escena.finalizar();
+    }
+
+    public void setEscena(Escena e){
+        this.escena = e;
+        this.escena.juego = this;
+        this.escena.consola = this.consola;
+        this.escena.inicializar();
+    }
+
+    public Escena getEscena(){
+        return this.escena;
+    }
+
+    public void detener(){
+        this.detener = true;
+    }
+
+    public int getAnchuraPantalla(){
+        return this.consola.
+    }
+    public int getAlturaPantalla(){
+        return this.consola.
+    }
+}
