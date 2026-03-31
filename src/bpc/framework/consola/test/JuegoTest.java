4d00646a5a354f5e55726bf775738e4bc7e3e827
Luis <luiscampillo07.com>
2026-03-31 11:44:24 +0200

    Creacion del test sin completar

---
+++ src/bpc/framework/consola/test/JuegoTest.java
@@ -0,0 +1,34 @@
+package bpc.framework.consola.test;
+
+import bpc.framework.consola.Juego;
+import bpc.framework.consola.Resolucion;
+
+public class JuegoTest {
+    public static void main(String[] args){
+        GameObjectTesteador objeto = new GameObjectTesteador();
+        EscenaTesteadora escenaTesteadora = new EscenaTesteadora(objeto);
+
+        Juego juego = new Juego();
+
+        Runnable juegoInicio = juego.iniciar(escenaTesteadora, Resolucion.VENTANA_VGA);
+        Thread juegoComienzo = new Thread(juegoInicio);
+        juegoComienzo.start();
+
+        try{
+            Thread.sleep(2000);
+            juego.detener();
+            juegoComienzo.join();
+
+            int frames = objeto.frameCounter();
+            System.out.println("Los frames ejecutados han sido: " + frames);
+
+            if(frames >= 110 && frames <= 130){
+                System.out.println("El test ha salido bien");
+            }else{
+                System.out.println("El tet ha salido mal");
+            }
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
