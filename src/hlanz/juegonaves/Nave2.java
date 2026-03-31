46f51b9283f53ffd2f8469ca1d363350db6dd592
Luis <luiscampillo07.com>
2026-03-31 11:44:09 +0200

    Creacion de las clases nave aun sin terminar

---
+++ src/hlanz/juegonaves/Nave2.java
@@ -0,0 +1,34 @@
+package hlanz.juegonaves;
+
+import bpc.framework.consola.GameObject;
+
+public class Nave2 extends Nave{
+    public Nave2(int ti, int tde, int tdi) {
+        super(ti, tde, tdi,3,);
+    }
+
+    @Override
+    protected void realizarDisparo() {
+
+    }
+
+    @Override
+    public void inicializar() {
+
+    }
+
+    @Override
+    public void añadir(GameObject obj) {
+
+    }
+
+    @Override
+    public void retirar(GameObject obj) {
+
+    }
+
+    @Override
+    public void finalizar() {
+
+    }
+}
