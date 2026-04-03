f2f1042f307ddc3842763ab01ddf0c1c785a18da
sarajimenezlara <sarajimlar207@gmail.com>
2026-04-04 00:19:07 +0200

    Añadir las imagenes requeridas de disparo y fondo en el proyecto, dejar casi terminado de programar disparo y empezar a programar el fondo

--- src/hlanz/juegonaves/Disparo.java
+++ src/hlanz/juegonaves/Disparo.java
@@ -1,5 +1,7 @@
 package hlanz.juegonaves;
 
+import bpc.framework.consola.Escena;
+import bpc.framework.consola.Resolucion;
 import bpc.framework.consola.SpriteGameObject;
 
 public class Disparo extends SpriteGameObject {
@@ -8,6 +10,23 @@ public class Disparo extends SpriteGameObject {
     private int vy;
 
     public Disparo(int id, int x, int y, int vx, int vy){
-        super();
+        this.idjugador=id;
+        super( ,x,y);
+        this.vx=vx;
+        this.vy=vy;
+    }
+    @Override
+    public void inicializar() {
+        this.escena.inicializar();
+    }
+    @Override
+    public void ejecutarFrame() {
+        this.moverX(vx);
+        this.moverY(vy);
+        if (vx*vy<Resolucion.PANTALLA_COMPLETA)
+    }
+    @Override
+    public void finalizar() {
+        this.escena.finalizar();
     }
 }
